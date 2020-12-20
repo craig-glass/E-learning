3198aee0d2969030eb99ba0c3e432e779c7d6abf
BuildTools <stuart.manfred.lewis@gmail.com>
2020-12-20 14:48:10 +0000

    Updated profile and added to admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import Profile
+from .forms import UserCreationForm, UserChangeForm
+
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('userid', 'first_name', 'last_name', 'is_superuser')
+    list_filter = ('is_superuser',)
+
+    fieldsets = (
+        (None, {'fields': ('userid', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_student', 'is_staff', 'is_superuser')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2')
+        })
+    )
+    search_fields = ('userid', 'email')
+    ordering = ('userid',)
+    filter_horizontal = ()
+
+
+admin.site.register(Profile, UserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,55 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 
-class Profile(AbstractUser):
+class ProfileManager(BaseUserManager):
+    def create_user(self, userid, email, password=None):
+        if not userid:
+            raise ValueError('Users must have an ID')
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, userid, email, password=None):
+        user = self.create_user(
+            userid,
+            password=password,
+            email=email,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class Profile(AbstractBaseUser):
+    userid = models.CharField(max_length=50, unique=True, null=False)
+
+    first_name = models.CharField(max_length=150, null=False)
+    last_name = models.CharField(max_length=150, blank=True, null=True)
+
     email = models.EmailField(max_length=254, unique=True, null=False)
-    phone_number = models.CharField(max_length=20)
-    term_address = models.CharField(max_length=150)
-    is_student = models.BooleanField(default=False)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    term_address = models.CharField(max_length=150, blank=True, null=True)
 
+    is_student = models.BooleanField(default=False, null=False)
+    is_staff = models.BooleanField(default=False, null=False)
+    is_superuser = models.BooleanField(default=False, null=False)
+
+    date_joined = models.DateTimeField(blank=True, null=True)
+
+    USERNAME_FIELD = 'userid'
     REQUIRED_FIELDS = ['email']
+
+    def __str__(self):
+        return self.userid
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
